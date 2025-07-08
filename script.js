28af0d098233ec13fc1ccb02600465af7d644bdd
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-07-08 17:04:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,24 +7,5 @@ search.addEventListener("click", () => {
     const city = input.value.trim();
     if (city === "") return;
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api_Key}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.cod === "404") {
-                alert("City not found")
-                return
-            }
-
-            result.classList.remove("hidden");
-            document.querySelector("#cityName").textContent = data.name;
-            document.querySelector("#description").textContent = data.weather[0].description;
-            document.querySelector("#temperature").textContent = `Temperature ${data.main.temp} C`;
-            document.querySelector("#humidity").textContent = `Humidity${data.main.humidity} %`;
-            document.querySelector("#wind").textContent = `Wind speed ${data.wind.speed} m/s`;
-            document.querySelector("#wheatherIcon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-        })
-        .catch(err => {
-            alert("Error fatching data")
-            console.error(err)
         })
 })
